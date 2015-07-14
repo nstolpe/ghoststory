01e36f20642c95041cfb74d3b88b68a4c836a3ab
nils <slincire@gmail.com>
2015-07-14 17:38:42 -0400

    added PlayScreen

---
+++ core/src/com/hh/ghoststory/screens/PlayScreen.java
@@ -0,0 +1,46 @@
+package com.hh.ghoststory.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.hh.ghoststory.GhostStory;
+
+/**
+ * Created by nils on 7/14/15.
+ */
+public class PlayScreen extends AbstractScreen {
+	public PlayScreen(GhostStory game) {
+		super(game);
+	}
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+	}
+
+	@Override
+	public void hide() {
+		super.hide();
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+}
