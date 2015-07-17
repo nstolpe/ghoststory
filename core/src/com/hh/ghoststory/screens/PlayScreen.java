8753f1206b0e72a04aea70d1ba214cb08c20c1d7
nils <nils@gangshi>
2015-07-17 04:51:30 -0400

    moved stuff from PlayScreen to DualCameraAbstractScreen

--- core/src/com/hh/ghoststory/screens/PlayScreen.java
+++ core/src/com/hh/ghoststory/screens/PlayScreen.java
@@ -2,6 +2,7 @@ package com.hh.ghoststory.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -18,38 +19,26 @@ import com.hh.ghoststory.utility.TestLoader;
  * Created by nils on 7/14/15.
  */
 public class PlayScreen extends DualCameraAbstractScreen {
-	private AssetManager assetManager = new AssetManager();
 	private ModelBatch modelBatch = new ModelBatch(Gdx.files.internal("shaders/default.vertex.glsl"), Gdx.files.internal("shaders/default.fragment.glsl"));
-	public Array<ModelInstance> instances = new Array<>();
-	private boolean loading;
 
 	private Environment environment = new Environment();
 
+	public PlayScreen(GhostStory game, Camera camera) {
+		super(game, camera);
+	}
 	public PlayScreen(GhostStory game) {
 		super(game);
-		setActiveCamera(new PerspectiveCamera());
-		camController = new CameraInputController(camera);
-		Gdx.input.setInputProcessor(camController);
-		// make the background purple so we know something is happening.
 		setClear(0.7f, 0.1f, 1f, 1);
 
 		// load assets. These should be pulled in through a config.
 		assetManager.load("models/ghost.g3dj", Model.class);
 		assetManager.load("models/tile.g3dj", Model.class);
 
+		// Test loader is for temporary data/assets/whatever
 		TestLoader.setLights(environment);
 		loading = true;
 	}
 
-	/**
-	 * Called when the asset manager has finished updating. Make models here.
-	 * @TODO Add more stuff that needs to happen after loading.
-	 */
-	public void doneLoading() {
-		instances = TestLoader.getTestModels(assetManager);
-
-		loading = false;
-	}
 	@Override
 	public void show() {
 	}
