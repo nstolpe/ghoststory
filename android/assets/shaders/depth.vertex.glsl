a899472ff14c58028808815d0d05949f5d5ad44e
nils <nils@gangshi>
2015-07-23 03:55:11 -0400

    formatted more lines

--- android/assets/shaders/depth.vertex.glsl
+++ android/assets/shaders/depth.vertex.glsl
@@ -17,8 +17,7 @@ uniform mat4 u_worldTrans;
 varying vec4 v_position;
 
 
-void main()
-{
+void main() {
     v_position = u_worldTrans * vec4(a_position, 1.0);
     gl_Position = u_projViewTrans * v_position;
 }
