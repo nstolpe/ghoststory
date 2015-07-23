c4e603a2619c402180524fd650839d247f525b6a
nils <nils@gangshi>
2015-07-23 03:51:56 -0400

    formatted a line

--- android/assets/shaders/depth.fragment.glsl
+++ android/assets/shaders/depth.fragment.glsl
@@ -17,7 +17,7 @@ uniform vec3 u_lightPosition;
 void main()
 {
 	// Simple depth calculation, just the length of the vector light-current position
-	gl_FragColor     = vec4(length(v_position.xyz-u_lightPosition)/u_cameraFar);
-	
+	gl_FragColor = vec4(length(v_position.xyz-u_lightPosition)/u_cameraFar);
+
 }
 
