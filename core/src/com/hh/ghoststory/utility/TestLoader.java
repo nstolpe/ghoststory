45c4f6c38f027b45f9c6706108307d8915031848
nils <slincire@gmail.com>
2015-07-22 20:54:39 -0400

    removed the tiles. last commit has a tag for them

--- core/src/com/hh/ghoststory/utility/TestLoader.java
+++ core/src/com/hh/ghoststory/utility/TestLoader.java
@@ -31,53 +31,6 @@ public class TestLoader {
 		instances.add(character);
 		instances.add(scene);
 
-//		10x10 grid on the ground.
-		for (int z = 0; z < 10; z++) {
-			for (int x = 0; x < 20; x++) {
-				tile = new ModelInstance(assetManager.get("models/tile.g3dj", Model.class));
-				tile.transform.setTranslation(x,0,z);
-//				instances.add(tile);
-			}
-		}
-//		builds the far side of the wall thingy, that can't be seen.
-		for (int z = 0; z < 3; z++) {
-			for (int y = 0; y < 2; y++) {
-				tile = new ModelInstance(assetManager.get("models/tile.g3dj", Model.class));
-				tile.transform.setToRotation(new Vector3(0, 0, 1), 90);
-				tile.transform.setTranslation(9.5f,y + 0.5f,z + 5);
-//				instances.add(tile);
-			}
-		}
-//		top of the little wall thing.
-		for (int z = 0; z < 3; z++) {
-			tile = new ModelInstance(assetManager.get("models/tile.g3dj", Model.class));
-			tile.transform.setTranslation(10, 2, z + 5);
-//			instances.add(tile);
-		}
-//		thin front of the wall thing.
-		for (int y = 0; y < 2; y++) {
-			tile = new ModelInstance(assetManager.get("models/tile.g3dj", Model.class));
-			tile.transform.setToRotation(new Vector3(1, 0, 0), 90);
-			tile.transform.setTranslation(10, y + 0.5f, 7.5f);
-//			instances.add(tile);
-		}
-//		thin back of wall
-		for (int y = 0; y < 2; y++) {
-			tile = new ModelInstance(assetManager.get("models/tile.g3dj", Model.class));
-			tile.transform.setToRotation(new Vector3(1, 0, 0), 270);
-			tile.transform.setTranslation(10, y + 0.5f, 4.5f);
-//			instances.add(tile);
-		}
-//		side of the wall thing, the big part.
-		for (int z = 0; z < 3; z++) {
-			for (int y = 0; y < 2; y++) {
-				tile = new ModelInstance(assetManager.get("models/tile.g3dj", Model.class));
-				tile.transform.setToRotation(new Vector3(0,0,1), 270);
-				tile.transform.setTranslation(10.5f, y + 0.5f, z + 5);
-//				instances.add(tile);
-			}
-		}
-
 		return instances;
 	}
 	public static void setLights(Environment environment) {
