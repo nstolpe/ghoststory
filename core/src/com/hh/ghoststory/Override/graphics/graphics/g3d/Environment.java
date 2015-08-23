5f60e803649999f4080be869b05a4979ce11e533
nils <slincire@gmail.com>
2015-08-23 01:35:13 -0400

    upgraded libgdx to 1.6.5

---
+++ core/src/com/hh/ghoststory/Override/graphics/graphics/g3d/Environment.java
@@ -0,0 +1,7 @@
+package com.hh.ghoststory.Override.graphics.graphics.g3d;
+
+/**
+ * Created by nils on 8/23/15.
+ */
+public class Environment {
+}
---
+++ core/src/com/hh/ghoststory/Override/graphics/graphics/g3d/shaders/DefaultShader.java
@@ -0,0 +1,7 @@
+package com.hh.ghoststory.Override.graphics.graphics.g3d.shaders;
+
+/**
+ * Created by nils on 8/23/15.
+ */
+public class DefaultShader {
+}
