b76de035b582f6ca86c4c717abff56708f91c4c0
nils <slincire@gmail.com>
2015-08-25 02:35:38 -0400

    cleaned up some code, maybe

--- android/assets/shaders/shadow.fragment.glsl
+++ android/assets/shaders/shadow.fragment.glsl
@@ -22,32 +22,36 @@ varying vec4 v_positionLightTrans;
 void main()
 {
 	// Default is to not add any color
-	float intensity=0.0; 
+	float intensity = 0.0;
 	// Vector light-current position
-	vec3 lightDirection=v_position.xyz-u_lightPosition;
-	float lenToLight=length(lightDirection)/u_cameraFar;
+	vec3 lightDirection = v_position.xyz-u_lightPosition;
+	float lenToLight=length(lightDirection) / u_cameraFar;
 	// By default assume shadow
-	float lenDepthMap=-1.0;
+	float lenDepthMap = -1.0;
 	
 	// Directional light, check if in field of view and get the depth
-	if(u_type==1.0){
-		vec3 depth = (v_positionLightTrans.xyz / v_positionLightTrans.w)*0.5+0.5;
-		if (v_positionLightTrans.z>=0.0 && (depth.x >= 0.0) && (depth.x <= 1.0) && (depth.y >= 0.0) && (depth.y <= 1.0) ) {
+	if(u_type == 1.0) {
+		vec3 depth = (v_positionLightTrans.xyz / v_positionLightTrans.w) * 0.5 + 0.5;
+		if (v_positionLightTrans.z >= 0.0 && (depth.x >= 0.0) && (depth.x <= 1.0) && (depth.y >= 0.0) && (depth.y <= 1.0)) {
 			lenDepthMap = texture2D(u_depthMapDir, depth.xy).a;
 		}
 	}
 	// Point light, just get the depth given light vector
-	else if(u_type==2.0){
+	else if(u_type == 2.0){
 		lenDepthMap = textureCube(u_depthMapCube, lightDirection).a;
 	}
 	
 	// If not in shadow, add some light
-	if(lenDepthMap<lenToLight-0.005){
-	}else{
-		intensity=0.5*(1.0-lenToLight);
+	if(lenDepthMap >= lenToLight - 0.005){
+		intensity = 0.5 * (1.0 - lenToLight);
 	}
+// not sure if below was here for some specific reason.
+//	if(lenDepthMap < lenToLight - 0.005){
+//	}else{
+//		intensity = 0.5 * (1.0 - lenToLight);
+//	}
 	
-	gl_FragColor     = vec4(intensity);
+	gl_FragColor = vec4(intensity);
 
 }
 
