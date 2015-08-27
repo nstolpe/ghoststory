9a89eb29f84860c483932e56cfd51cc0619fde61
nils <slincire@gmail.com>
2015-08-27 04:23:54 -0400

    added some Tweens and Timelines.  is getting less abstract.

--- core/src/com/hh/ghoststory/Tween/Timelines.java
+++ core/src/com/hh/ghoststory/Tween/Timelines.java
@@ -1,36 +1,43 @@
 package com.hh.ghoststory.Tween;
 
 import aurelienribon.tweenengine.*;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+
 import java.util.Random;
 
 /**
  * Class to hold TweenCallbacks.
- * Example of using these tweens as callbacks:
+ *
+ * Using these functions to get new Timelines:
+ *
+ * Timeline bounce = Timelines.bounce(pointLight.position, Vector3Accessor.POSITION_XYZ, 1, new Vector3(0,0,0), new Vector3(10,0,10), TweenEquations.easeInSine);
+ * bounce.start(tweenManager);
+ *
+ * Example of using these tweens as looping callbacks:
  *
  * TweenCallback callback = new TweenCallback() {
  *     @Override
  *     public void onEvent(int type, BaseTween<?> source) {
- *         Timeline timeline = bounce(tweenable, accessor, duration, start, finish, easing);
+ *         Timeline timeline = Timelines.bounce(modelInstance.position, Vector3Accessor.POSITION_XYZ, 1, new Vector3(0,0,0), new Vector3(10,0,10), TweenEquations.easeInSine);
  *         timeline.setCallback(this);
- *         timeline.start(manager);
+ *         timeline.start(tweenManager);
  *     }
  * };
  *
  * TweenCallback callback = new TweenCallback() {
  *     @Override
  *     public void onEvent(int type, BaseTween<?> source) {
- *         Timeline timeline = random(tweenable, accessor, duration, easing, count);
+ *         Timeline timeline = Timelines.random(tweenable, ColorAccessor.COLORS, 1, TweenEquations.easeNone, count);
  *         timeline.setCallback(this);
- *         timeline.start(manager);
+ *         timeline.start(tweenManager);
  *     }
  * };
  *
  */
 public class Timelines {
-
-
 	/**
-	 * Returns a tween Timeline that will make an object move between
+	 * Returns a Tween Timeline that will make an object move between
 	 * two arrays (start and finish). Good for looping endlessly w/ callbacks.
 	 *
 	 * @param tweenable  Object that will be tweened.
@@ -68,5 +75,28 @@ public class Timelines {
 
 		timeline.push(Tween.to(tweenable, accessor, duration).target(target).ease(easing));
 		return timeline;
+	}
+
+	/**
+	 * Returns a timeline with one rotate Tween and one translate Tween. It will cause
+	 * its target to face a point and move towards it. Meant to be used on actors, which
+	 * will have a position Quaternion and a rotation Vector3. They haven't been built yet.
+	 * Use to move an actor that will first turn to face a target location, then move
+	 * towards it.
+	 *
+	 * @param rotation        Current rotation of the Actor being tweened.
+	 * @param rotationTarget    Target rotation of the Actor being tweened.
+	 * @param rotationDuration    Duration the rotation tween will last.
+	 *
+	 * @param translation      Current translation of the Actor being tweened.
+	 * @param translationTarget  Target translation of the Actor being tweened.
+	 * @param translationDuration  Duration the translation tween will last.
+	 * @return A configured Timeline.
+	 */
+	public static Timeline faceAndGoto(Quaternion rotation, Quaternion rotationTarget, float rotationDuration, Vector3 translation, Vector3 translationTarget, float translationDuration) {
+		Tween rotate = Tweens.rotate(rotationDuration, TweenEquations.easeNone, rotation, rotationTarget);
+		Tween translate = Tweens.translate(translationDuration, TweenEquations.easeNone, translation, translationTarget);
+
+		return Timeline.createSequence().push(rotate).push(translate);
 	}
 }
--- core/src/com/hh/ghoststory/Tween/Tweens.java
+++ core/src/com/hh/ghoststory/Tween/Tweens.java
@@ -1,7 +1,32 @@
 package com.hh.ghoststory.Tween;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenEquation;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+
 /**
- * Created by nils on 8/27/15.
+ * Class to store often used tweens.
  */
 public class Tweens {
+	public static Tween rotate(float duration, final TweenEquation easing, Quaternion current, Quaternion target) {
+		float[] targets = new float[4];
+
+		targets[0] = target.x;
+		targets[1] = target.y;
+		targets[2] = target.z;
+		targets[3] = target.w;
+
+		return Tween.to(current, QuaternionAccessor.ROTATION, duration).target(targets).ease(easing);
+	}
+
+	public static Tween translate(float duration, final TweenEquation easing, Vector3 current, Vector3 target) {
+		float[] targets = new float[3];
+
+		targets[0] = target.x;
+		targets[1] = target.y;
+		targets[2] = target.z;
+
+		return Tween.to(current, Vector3Accessor.POSITION_XYZ, duration).target(targets).ease(easing);
+	}
 }
