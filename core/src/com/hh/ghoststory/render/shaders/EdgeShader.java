5b07a572be24df5030851572db3ee5162e01aad7
nils <slincire@gmail.com>
2015-10-08 16:21:08 -0400

    test case

---
+++ core/src/com/hh/ghoststory/render/shaders/EdgeShader.java
@@ -0,0 +1,7 @@
+package com.hh.ghoststory.render.shaders;
+
+/**
+ * Created by nils on 10/8/15.
+ */
+public class EdgeShader {
+}
---
+++ core/src/com/hh/ghoststory/screen/TestScreen.java
@@ -0,0 +1,98 @@
+package com.hh.ghoststory.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.hh.ghoststory.GhostStory;
+import com.hh.ghoststory.ScreenshotFactory;
+
+/**
+ * Created by nils on 10/8/15.
+ */
+public class TestScreen extends AbstractScreen {
+	private SpriteBatch spriteBatch = new SpriteBatch();
+	private ModelBatch modelBatch = new ModelBatch();
+
+	private AssetManager assets = new AssetManager();
+	private PerspectiveCamera mainCamera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	private OrthographicCamera spriteCamera = new OrthographicCamera();
+
+	private FrameBuffer frameBuffer1;
+	private FrameBuffer frameBuffer2;
+
+	private Texture tmpTexture;
+	private ModelInstance instance;
+
+	public TestScreen(GhostStory game) {
+		super(game);
+		initFBOS();
+		assets.load("models/ghost_blue.g3dj", Model.class);
+		mainCamera.position.set(5, 5, 5);
+		mainCamera.lookAt(0, 0, 0);
+		mainCamera.near = 1;
+		mainCamera.far = 300;
+		mainCamera.update();
+	}
+
+	@Override
+	public void render(float delta) {
+		mainCamera.update();
+
+		if (assets.update() && instance == null)
+			instance = new ModelInstance(assets.get("models/ghost_blue.g3dj", Model.class));
+
+		if (instance != null) {
+			frameBuffer1.begin();
+
+			Gdx.gl.glClearColor(1, 0, 0, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+			modelBatch.begin(mainCamera);
+			modelBatch.render(instance);
+			modelBatch.end();
+
+			frameBuffer1.end();
+
+			tmpTexture = frameBuffer1.getColorBufferTexture();
+
+			spriteCamera.setToOrtho(false, frameBuffer1.getWidth(), frameBuffer1.getWidth());
+			spriteBatch.setProjectionMatrix(spriteCamera.combined);
+			spriteBatch.begin();
+
+			Gdx.gl.glClearColor(0, 0, 1, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+			spriteBatch.draw(tmpTexture, 0, 0, 200, 200);
+			spriteBatch.end();
+			ScreenshotFactory.saveScreenshot(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), "edge");
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		initFBOS();
+	}
+
+	@Override
+	public void dispose() {
+		frameBuffer1.dispose();
+		frameBuffer2.dispose();
+		spriteBatch.dispose();
+		modelBatch.dispose();
+		tmpTexture.dispose();
+		assets.dispose();
+	}
+
+	private void initFBOS() {
+		if (frameBuffer1 != null) frameBuffer1.dispose();
+			frameBuffer1 = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+
+		if (frameBuffer2 != null) frameBuffer2.dispose();
+			frameBuffer2 = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+	}
+}
