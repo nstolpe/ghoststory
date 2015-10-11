0b283b592ff1aa2666c6f341fdc5e8a45afa88b2
nils <slincire@gmail.com>
2015-10-11 14:40:29 -0400

    testing weird stencil buffer issue

--- core/src/com/hh/ghoststory/screen/TestScreen.java
+++ core/src/com/hh/ghoststory/screen/TestScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.hh.ghoststory.GhostStory;
 import com.hh.ghoststory.ScreenshotFactory;
 
@@ -19,11 +20,18 @@ public class TestScreen extends AbstractScreen {
 	private SpriteBatch spriteBatch = new SpriteBatch();
 	private ModelBatch modelBatch = new ModelBatch();
 	public ModelBatch outlineBatch = new ModelBatch(
-		Gdx.files.internal("shaders/default.vertex.glsl").readString(),
+//		Gdx.files.internal("shaders/default.vertex.glsl").readString(),
+		"attribute vec4 a_position;\n" +
+		"uniform mat4 u_projViewTrans;\n" +
 		"void main()\n" +
-			"{\n" +
-			"    gl_FragColor = vec4(0.04, 0.28, 0.26, 1.0);\n" +
-			"}");
+		"{\n" +
+		"    gl_Position =  u_projViewTrans * a_position;\n" +
+		"}",
+		"void main()\n" +
+		"{\n" +
+		"    gl_FragColor = vec4(0.04, 0.28, 0.26, 1.0);\n" +
+		"}"
+	);
 
 	private AssetManager assets = new AssetManager();
 	private PerspectiveCamera mainCamera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -61,6 +69,7 @@ public class TestScreen extends AbstractScreen {
 		if (instance != null) {
 			int mode = 1;
 			switch (mode) {
+				// multiple frame buffers
 				case 0:
 					frameBuffer1.begin();
 
@@ -85,28 +94,59 @@ public class TestScreen extends AbstractScreen {
 					spriteBatch.draw(tmpTexture, 0, 0, 200, 200);
 					spriteBatch.end();
 					break;
+				// stencil
 				case 1:
 					Gdx.gl.glClearColor(1, 0, 0, 1);
-					Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+					Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 					Gdx.gl.glEnable(GL20.GL_STENCIL_TEST);
-					Gdx.gl.glStencilFunc(GL20.GL_ALWAYS, 1, 1);
 					Gdx.gl.glStencilOp(GL20.GL_KEEP, GL20.GL_KEEP, GL20.GL_REPLACE);
-					Gdx.gl.glStencilMask(1);
-					Gdx.gl.glClear(GL20.GL_STENCIL_BUFFER_BIT);
+					Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | GL20.GL_STENCIL_BUFFER_BIT);
+
+					Gdx.gl.glStencilFunc(GL20.GL_ALWAYS, 1, 0xFF);
+					Gdx.gl.glStencilMask(0xFF);
+
 					modelBatch.begin(mainCamera);
 					modelBatch.render(instance);
 					modelBatch.end();
 
-					Gdx.gl.glStencilFunc(GL20.GL_EQUAL, 0, 1);
-					Gdx.gl.glStencilOp(GL20.GL_KEEP, GL20.GL_KEEP, GL20.GL_KEEP);
+					Gdx.gl.glStencilFunc(GL20.GL_NOTEQUAL, 1, 0xFF);
 					Gdx.gl.glStencilMask(0x00);
+					Gdx.gl.glDisable(GL20.GL_DEPTH_TEST);
+
 					instance.transform.scl(1.1f);
 					outlineBatch.begin(mainCamera);
 					outlineBatch.render(instance);
 					outlineBatch.end();
 					instance.transform.scl(100f / 110f);
-					Gdx.gl.glDisable(GL20.GL_STENCIL_TEST);
+
+					Gdx.gl.glStencilMask(0xFF);
+					Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+
+
+//					Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | GL20.GL_STENCIL_BUFFER_BIT);
+//
+//					Gdx.gl.glEnable(GL20.GL_STENCIL_TEST);
+//					Gdx.gl.glStencilFunc(GL20.GL_ALWAYS, 1, 1);
+//					Gdx.gl.glStencilOp(GL20.GL_KEEP, GL20.GL_KEEP, GL20.GL_REPLACE);
+//					Gdx.gl.glStencilMask(0x00);
+//					Gdx.gl.glClear(GL20.GL_STENCIL_BUFFER_BIT);
+//
+//					modelBatch.begin(mainCamera);
+//					modelBatch.render(instance);
+//					modelBatch.end();
+//
+//					Gdx.gl.glStencilFunc(GL20.GL_EQUAL, 0, 1);
+//					Gdx.gl.glStencilOp(GL20.GL_KEEP, GL20.GL_KEEP, GL20.GL_KEEP);
+//					Gdx.gl.glStencilMask(0x00);
+//
+//					instance.transform.scl(1.1f);
+//					outlineBatch.begin(mainCamera);
+//					outlineBatch.render(instance);
+//					outlineBatch.end();
+//					instance.transform.scl(100f / 110f);
+//
+//					Gdx.gl.glDisable(GL20.GL_STENCIL_TEST);
 					break;
 				default:
 					break;
