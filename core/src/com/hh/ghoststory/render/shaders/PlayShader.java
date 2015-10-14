788685ee7470901557c567bd8132c3142ecd74b3
nils <slincire@gmail.com>
2015-10-14 02:50:15 -0400

    removed commented code

--- core/src/com/hh/ghoststory/render/shaders/PlayShader.java
+++ core/src/com/hh/ghoststory/render/shaders/PlayShader.java
@@ -19,11 +19,6 @@ public class PlayShader extends DefaultShader {
 //	private static final String vertex = Gdx.files.internal("shaders/default.vertex.glsl").readString();
 //	private static final String fragment = Gdx.files.internal("shaders/default.fragment.glsl").readString();
 	private static final Config config = new Config(Gdx.files.internal("shaders/default.vertex.glsl").readString(), Gdx.files.internal("shaders/default.fragment.glsl").readString());
-	public boolean alpha = false;
-
-	public void alpha(boolean alpha) {
-		this.alpha = alpha;
-	}
 
 	public PlayShader(Renderable renderable) {
 		super(renderable, config, createPrefix(renderable, config));
@@ -38,33 +33,8 @@ public class PlayShader extends DefaultShader {
 	}
 
 	public static String createPrefix(final Renderable renderable, final Config config) {
-//		final Attributes attributes = combineAttributes(renderable);
-//		final Environment lights = renderable.environment;
-//		final DirectionalLightsAttribute dla = attributes.get(DirectionalLightsAttribute.class, DirectionalLightsAttribute.Type);
-//		final Array<DirectionalLight> dirs = dla == null ? null : dla.lights;
-//		final PointLightsAttribute pla = attributes.get(PointLightsAttribute.class, PointLightsAttribute.Type);
-//		final Array<PointLight> points = pla == null ? null : pla.lights;
-//		final SpotLightsAttribute sla = attributes.get(SpotLightsAttribute.class, SpotLightsAttribute.Type);
-//		final Array<SpotLight> spots = sla == null ? null : sla.lights;
 		String prefix = DefaultShader.createPrefix(renderable, config);
-		// For each light: light position, camera.far
-		// need depthmap size (i think)
-		// and the sampler (u_shadows, already have).
-		//
-		// use all that to process the pcf and interpolation and let's see how it works.
-		//
-		// For each light w/ casting enabled,
-		// set a light position array (if it's directional or spot we need facing vec too, but do that later)
-		//
-		// Look at shadow.fragment.glsl for handling of point vs spot lights. The handling of PCF/VSM might be better done there,
-		// then sample it here like usual.
 		if (renderable.material.has(AlphaAttribute.ID)) prefix += "#define alphaFlag\n";
-
-//		if (alphaInstance != null) {
-//			Renderable ren = new Renderable();
-//			alphaInstance.getRenderable(ren);
-//			if (ren == renderable) prefix += "#define alphaFlag\n";
-//		}
 		return prefix;
 	}
 
